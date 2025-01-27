caf119cb24038880e22f1d2de9aad110a0d721c7
odarink <holdzhgonov.a@gmail.com>
2025-01-27 08:54:52 +0300

    added cdm ddl

---
+++ ddl/cdm/cdm_sales_dboard.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS sys3.cdm_sales_dboard
+(
+    дата Date,
+    название_товара String,
+    название_категории String,
+    номер_заказа UInt32
+    сумма_продаж Decimal(18,2),
+    стоимость_рекламы Decimal(18,2)
+) ENGINE = MergeTree()
+PARTITION BY (дата)
+ORDER BY (номер_товара);
